9620f7c89daa5b09952a68bdfafee7e3229a91c6
Nilay Anurag <nilayanurag@gmail.com>
2023-11-15 20:56:53 -0500

    Commiting Directory structure

---
+++ config/mongoCollections.js
@@ -0,0 +1,21 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// Note: You will need to change the code below to have the collection required by the assignment!
+export const users = getCollectionFn('users');
+export const reviews = getCollectionFn('reviews');
+export const categories = getCollectionFn('categories');
+export const businesses = getCollectionFn('businesses');
+export const comments = getCollectionFn('comments');
