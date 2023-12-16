9d1aaf9c99c81174fdcb79d8f90537e039ac0f96
nancyradadia <narofficial08@gmail.com>
2023-12-16 03:32:02 -0500

    Seed file completed

--- seeds/seedComment.js
+++ seeds/seedComment.js
@@ -2,23 +2,143 @@ import * as reviewFunction from "../data/review.js";
 import * as userFunction from "../data/users.js";
 import * as commentFunction from "../data/comments.js";
 
-
 export const seedAllComment = async () => {
-    try {
+  const getReview = await reviewFunction.getAllReviews();
+  const getUser = await userFunction.getAllUsers();
+  try {
+    const testUser = getUser[0]._id.toString();
+    const testReview = getReview[0]._id.toString();
 
-        const getUser = await userFunction.getAllUsers();
-        const testUser = getUser[0]._id.toString();
-        const getReview = await reviewFunction.getAllReviews();
-        const testReview = getReview[0]._id.toString();
-    
-        const testComment = await commentFunction.createComment( testReview, testUser, "This is a test comment");
-        if (testComment.insertedComment) {
-            console.log("Comment seeded");
-        } else {
-            console.log("Comment not seeded");
-        }
-        
-    } catch (error) {
-        throw error;
-    }    
-}
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "I agree with the review posted."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[1]._id.toString();
+    const testReview = getReview[1]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "I completely agree with the review posted."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[2]._id.toString();
+    const testReview = getReview[2]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "What Non-sense review is this?"
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[3]._id.toString();
+    const testReview = getReview[3]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "I mean okay review but I don't agree with it completely."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[4]._id.toString();
+    const testReview = getReview[4]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "Sure, whatever you say."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[5]._id.toString();
+    const testReview = getReview[5]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "I agree with the review posted."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[6]._id.toString();
+    const testReview = getReview[6]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "Really? I don't think so."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const testUser = getUser[0]._id.toString();
+    const testReview = getReview[7]._id.toString();
+
+    const testComment = await commentFunction.createComment(
+      testReview,
+      testUser,
+      "Ofcourse, I agree with the review posted."
+    );
+    if (testComment.insertedComment) {
+      console.log("Comment seeded");
+    } else {
+      console.log("Comment not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
--- seeds/seedReview.js
+++ seeds/seedReview.js
@@ -3,156 +3,285 @@ import * as userFunctions from "../data/users.js";
 import * as businessFunctions from "../data/business.js";
 
 export const seedAllReview = async () => {
-    try {
-        const business = await businessFunctions.getBusinessByName("Napoli's Pizzeria");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Nancy");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            4,
-            "The ambience of Napoli's pizzeria was amazing and food was delightful as well. A must visit resto",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-    try {
-        const business = await businessFunctions.getBusinessByName("Patel Brothers");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Nancy");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            5,
-            "The Shopping experience of Indian groceries. A must visit grocery shop",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-    try {
-        const business = await businessFunctions.getBusinessByName("Chipotle");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Nilay");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            4,
-            "The tatse of the restraunt was fabulous specially the burrito bowl.",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-    try {
-        const business = await businessFunctions.getBusinessByName("Cinema");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Nilay");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            5,
-            "The Movie Experience was amazing in heart of manhattan",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-    try {
-        const business = await businessFunctions.getBusinessByName("Cinema");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Dev");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            5,
-            "The Movie Experience was amazing in heart of manhattan. New York Vibes",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-    try {
-        const business = await businessFunctions.getBusinessByName("Grocery");
-        const businessId = business._id.toString();
-        const categoryId = business.categoryId.toString();
-        const user = await userFunctions.getUserByUsername("Raj");
-        const userId = user[0]._id.toString();
-    
-        const review = await reviewFunctions.createReview(
-            businessId,
-            userId,
-            categoryId,
-            5,
-            "Great Shopping time",
-            "./public/images/testImage.png"
-        );
-        if(review.insertedReview){
-            console.log("Review seeded");
-        }else{
-            console.log("Review not seeded");
-        }
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const business = await businessFunctions.getBusinessByName(
+      "Napoli's Pizzeria"
+    );
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("nancyradadia");
+    const userId = user[0]._id.toString();
 
-  const review = await reviewFunctions.createReview(
-    businessId,
-    userId,
-    categoryId,
-    4,
-    "The ambience of Napoli's pizzeria was amazing and food was delightful as well. A must visit resto",
-    "./public/images/testImage.png"
-  );
-  if (review.insertedReview) {
-    console.log("Review seeded");
-  } else {
-    console.log("Review not seeded");
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      4,
+      "The ambience of Napoli's pizzeria was amazing and food was delightful as well. A must visit resto",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
   }
+  try {
+    const business = await businessFunctions.getBusinessByName(
+      "Patel Brothers"
+    );
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("nancyradadia");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      5,
+      "The Shopping experience of Indian groceries. A must visit grocery shop",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Chipotle");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("hitarthpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      4,
+      "The tatse of the restraunt was fabulous specially the burrito bowl.",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("AMC Newport");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("hitarthpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      5,
+      "The Movie Experience was amazing in heart of manhattan",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("AMC Empire 25");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("prashilpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      5,
+      "The Movie Experience was amazing in heart of manhattan. New York Vibes",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Heights Fitness");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("prashilpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      4,
+      "Too costly but the gym is amazing. The trainers are very helpful and the equipments are very good. A must visit gym",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    throw error;
+  }
+
+  try {
+    const business = await businessFunctions.getBusinessByName("Planet Fitness");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("harvishJ");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      1,
+      "I would not recommend this gym to anyone. The trainers are not helpful and the equipments are not good. A must not visit gym",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Hoboken Park");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("harvishJ");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      4,
+      "It is a very good park. The park is very clean and the equipments are very good. A must visit park",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("6th Borough Salon");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("nilayanurag");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      2,
+      "The salon is not good. The staff is not good and the equipments are not good. A must not visit salon",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Honest");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("nilayanurag");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      4,
+      "The food is very good. The staff is very good and the equipments are very good. A must visit restaurant",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Hoboken Park");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("rajpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      3,
+      "The park is very good. The park is very clean and the equipments are very good. A must visit park",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  try {
+    const business = await businessFunctions.getBusinessByName("Chipotle");
+    const businessId = business._id.toString();
+    const categoryId = business.categoryId.toString();
+    const user = await userFunctions.getUserByUsername("rajpatel");
+    const userId = user[0]._id.toString();
+
+    const review = await reviewFunctions.createReview(
+      businessId,
+      userId,
+      categoryId,
+      5,
+      "Best burrito bowl in town. The staff is very good and the equipments are very good. A must visit restaurant",
+      "./public/images/testImage.png"
+    );
+    if (review.insertedReview) {
+      console.log("Review seeded");
+    } else {
+      console.log("Review not seeded");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
